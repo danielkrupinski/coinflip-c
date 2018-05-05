da01b0efcd1cd391b01d2833122f06e50c3993e8
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-05 18:12:32 +0200

    Add struct Data

--- include/coinflip.h
+++ include/coinflip.h
@@ -1,6 +1,13 @@
 #ifndef COINFLIP_H
 #define COINFLIP_H
 
+struct Data {
+    int* heads;
+    int headsSize;
+    int* tails;
+    int tailsSize;
+};
+
 int inputData(void);
 void generateRandom(int, int*, int, int*, int);
 
