9ffe142becf8e68e392f777158459ed7417eb461
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-03 21:06:00 +0200

    Remove struct Data

--- include/coinflip.h
+++ include/coinflip.h
@@ -1,13 +1,7 @@
 #ifndef COINFLIP_H
 #define COINFLIP_H
 
-struct Data {
-    int amount;
-    int heads[amount];
-    int tails[amount];
-};
-
 int inputData(void);
-void generateRandom(struct Data*);
+void generateRandom(int);
 
 #endif // COINFLIP_H
